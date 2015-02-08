7caf89b9729d1d1a56aad17a070210c3daed2df8
luuhoabk <luuhoabk.developer@gmail.com>
2015-02-08 00:48:49 -0800

    Start hostgator

---
+++ logs/joomla_update.php
@@ -0,0 +1,17 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-02-08 07:52:22 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2015-02-08T07:52:22+00:00	INFO	update	Update started by user Super User (990). Old version is 3.3.0.
+2015-02-08T07:52:23+00:00	INFO	update	Downloading update file from .
+2015-02-08T07:52:33+00:00	INFO	update	File Joomla_3.3.x_to_3.3.6-Stable-Patch_Package.zip successfully downloaded.
+2015-02-08T07:52:33+00:00	INFO	update	Starting installation of new version.
+2015-02-08T07:53:02+00:00	INFO 127.0.0.1	update	Finalising installation.
+2015-02-08T07:53:04+00:00	INFO 127.0.0.1	update	Ran query from file 3.3.4-2014-08-03. Query text: ALTER TABLE `#__user_profiles` CHANGE `profile_value` `profile_value` TEXT NOT N.
+2015-02-08T07:53:04+00:00	INFO 127.0.0.1	update	Ran query from file 3.3.6-2014-09-30. Query text: INSERT INTO `#__update_sites` (`name`, `type`, `location`, `enabled`) VALUES ('J.
+2015-02-08T07:53:04+00:00	INFO 127.0.0.1	update	Ran query from file 3.3.6-2014-09-30. Query text: INSERT INTO `#__update_sites_extensions` (`update_site_id`, `extension_id`) VALU.
+2015-02-08T07:53:04+00:00	INFO 127.0.0.1	update	Deleting removed files and folders.
+2015-02-08T07:53:13+00:00	INFO 127.0.0.1	update	Cleaning up after installation.
+2015-02-08T07:53:13+00:00	INFO 127.0.0.1	update	Update to version 3.3.6 is complete.
