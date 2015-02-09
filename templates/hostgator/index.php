e66c5240f70ca4b8f2e8dd498516e8bd83650d52
luuhoabk <luuhoabk.developer@gmail.com>
2015-02-08 23:15:39 -0800

    [Home page] - 90 %

--- templates/hostgator/index.php
+++ templates/hostgator/index.php
@@ -38,9 +38,13 @@ else
 // Add JavaScript Frameworks
 JHtml::_('bootstrap.framework');
 $doc->addScript('templates/' . $this->template . '/js/template.js');
+//$doc->addScript
 
 // Add Stylesheets
+$doc->addStyleSheet($this->baseurl . '/media/jui/css/bootstrap.min.css');
+$doc->addStyleSheet('templates/' . $this->template . '/css/boostrap.min.css');
 $doc->addStyleSheet('templates/' . $this->template . '/css/template.css');
+$doc->addStyleSheet('templates/' . $this->template . '/css/hostgator.css');
 
 // Load optional RTL Bootstrap CSS
 JHtml::_('bootstrap.loadCss', false, $this->direction);
@@ -78,7 +82,9 @@ else
 }
 ?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:jdoc="http://www.w3.org/1999/XSL/Transform"
+	  xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>"
+	  dir="<?php echo $this->direction; ?>">
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<jdoc:include type="head" />
@@ -121,7 +127,7 @@ else
 	<![endif]-->
 </head>
 
-<body class="site <?php echo $option
+<body class="<?php echo $option
 	. ' view-' . $view
 	. ($layout ? ' layout-' . $layout : ' no-layout')
 	. ($task ? ' task-' . $task : ' no-task')
@@ -131,68 +137,178 @@ else
 
 	<!-- Body -->
 	<div class="body">
-		<div class="container<?php echo ($params->get('fluidContainer') ? '-fluid' : ''); ?>">
-			<!-- Header -->
-			<header class="header" role="banner">
-				<div class="header-inner clearfix">
-					<a class="brand pull-left" href="<?php echo $this->baseurl; ?>">
-						<?php echo $logo; ?>
-						<?php if ($this->params->get('sitedescription')) : ?>
-							<?php echo '<div class="site-description">' . htmlspecialchars($this->params->get('sitedescription')) . '</div>'; ?>
-						<?php endif; ?>
-					</a>
-					<div class="header-search pull-right">
-						<jdoc:include type="modules" name="position-0" style="none" />
-					</div>
-				</div>
-			</header>
-			<?php if ($this->countModules('position-1')) : ?>
-				<nav class="navigation" role="navigation">
-					<jdoc:include type="modules" name="position-1" style="none" />
-				</nav>
-			<?php endif; ?>
-			<jdoc:include type="modules" name="banner" style="xhtml" />
-			<div class="row-fluid">
-				<?php if ($this->countModules('position-8')) : ?>
-					<!-- Begin Sidebar -->
-					<div id="sidebar" class="span3">
-						<div class="sidebar-nav">
-							<jdoc:include type="modules" name="position-8" style="xhtml" />
+		<!-- Header -->
+		<header class="header header-inner" role="banner">
+
+			<div class="header-top"">
+				<div class="container">
+					<div class="row">
+						<div class="span2"></div>
+						<div class="span10 margin-left-0">
+							<img class="header-top-logo img-responsive" style="margin-bottom: -30px; margin-left: -10px; z-index: 10; position: relative;" src="../templates/hostgator/images/header-text-hostgator.png" alt=""/>
+							<img class="header-top-logo img-responsive margin-tb-10" style="margin-left: -35px;" src="../templates/hostgator/images/header-text-coupon-best.png" alt=""/>
 						</div>
 					</div>
-					<!-- End Sidebar -->
-				<?php endif; ?>
-				<main id="content" role="main" class="<?php echo $span; ?>">
-					<!-- Begin Content -->
-					<jdoc:include type="modules" name="position-3" style="xhtml" />
-					<jdoc:include type="message" />
-					<jdoc:include type="component" />
-					<jdoc:include type="modules" name="position-2" style="none" />
-					<!-- End Content -->
-				</main>
-				<?php if ($this->countModules('position-7')) : ?>
-					<div id="aside" class="span3">
-						<!-- Begin Right Sidebar -->
-						<jdoc:include type="modules" name="position-7" style="well" />
-						<!-- End Right Sidebar -->
-					</div>
-				<?php endif; ?>
+
+
+				</div>
+
 			</div>
-		</div>
-	</div>
+			<div class="header-bottom">
+				<div class="container"  style="position: relative;">
+					<di class="row-fluid">
+						<div class="span2">
+							<img style="position: relative; margin-top: -99px; bottom: 0;" class="header-top-logo img-responsive" src="../templates/hostgator/images/logo-dragon.png" alt=""/>
+						</div>
+						<div class="span9 clearfix">
+							<?php if ($this->countModules('position-1')) : ?>
+								<nav class="navigation margin-bottom-0" role="navigation">
+									<jdoc:include type="modules" name="position-1" style="none" />
+								</nav>
+							<?php endif; ?>
+						</div>
+					</di>
+
+				</div>
+			</div>
+		</header>
+<!-------------------------------------------------------->
+<!--		<jdoc:include type="modules" name="banner" style="xhtml" />-->
+		<aritcle>
+			<div class="wrapper-body">
+				<div class="container">
+					<div class="row">
+						<div class="span8 col-xs-8">
+							<div class="body-left" style="height: 500px;">
+								<div class="span5" style="color: #fff; margin-top: 180px; margin-left: 130px;">
+									<h2 style="font-size: 2.4vw;"><span style="color: #fdc04c;">HostGator</span> WebHosting</h2>
+									<p style="text-align: justify; font-size: 0.9vw;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morlci non pretium metus, sit amet cursus nisi. Nam turpis, rhoncus curus iaculi a Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit amet cursus nisi. Nam turpis,</p>
+								</div>
+								<div class="span4 text-center" style="margin-left: 185px; margin-top: 30px; color: #fff;">
+									<h2 style="font-size: 3em;">HGTR17</h2>
+									<p style="font-size: 1.2em; font-weight: bold;">60 Days Free Money Back Guarantee</p>
+								</div>
+							</div>
+						</div>
+						<div class="span4 col-xs-4">
+							<div class="row body-right">
+								<ul>
+									<li class="box-right-1">
+										<div class="row margin-left-0">
+											<div class="item-wrapper">
+												<h2 class="margin-0">GTREKT46</h2>
+												<p>60% off in your next purchase</p>
+											</div>
+										</div>
+									</li>
+									<li class="box-right-2">
+										<div class="row margin-left-0 item-wrapper">
+											<div class="item-wrapper">
+												<h2 class="margin-0">MAMAMO14</h2>
+												<p>60% off in your next purchase</p>
+											</div>
+										</div>
+									</li>
+									<li class="box-right-3">
+										<div class="row margin-left-0 item-wrapper">
+											<div class="item-wrapper">
+												<h2 class="margin-0">RYUKSR2</h2>
+												<p>60% off in your next purchase</p>
+											</div>
+										</div>
+									</li>
+									<li class="box-right-4">
+										<div class="row margin-left-0 item-wrapper">
+											<div class="item-wrapper">
+												<h2 class="margin-0">MAMAMO15</h2>
+												<p>60% off in your next purchase</p>
+											</div>
+										</div>
+									</li>
+									<li class="box-right-5">
+										<div class="row margin-left-0 item-wrapper">
+											<div class="item-wrapper">
+												<h2 class="margin-0">NYAWRO7</h2>
+												<p>60% off in your next purchase</p>
+											</div>
+										</div>
+									</li>
+								</ul>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</aritcle>
+<!-------------------------------------------------------->
+		<article>
+			<div class="row text-center" style="background-color: #0171bc;" >
+				<h1 style="font-weight: bold; color: #fff;">About <span style="color: #fdc04c;"> HostGator </span><span style="font-size: 18px;"> WebHosting</span></h1>
+			</div>
+		</article>
+<!-------------------------------------------------------->
+		<article>
+			<div class="row body-box-bottom">
+				<div class="container">
+					<div class="row text-center body-box-bottom-note">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morlci non pretium metus, sit amet cursus nisi</p>
+						<p>Nam turpis, rhoncus curus iaculi a</p>
+					</div>
+					<div class="row body-box-bottom-item">
+						<div class="span4">
+							<div class="item-img text-center">
+								<img class="header-top-logo img-responsive" src="../templates/hostgator/images/icon-bottom-1.png" alt=""/>
+							</div>
+							<div class="item-text text-center">
+								<h2>Lorem Lipsum</h2>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morlci non pretium metus, sit amet cursus nisi. Nam turpis, rhoncus curus iaculi a
+								</p>
+							</div>
+						</div>
+						<div class="span4 body-box-bottom-item">
+							<div class="item-img text-center">
+								<img class="header-top-logo img-responsive" src="../templates/hostgator/images/icon-bottom-2.png" alt=""/>
+							</div>
+							<div class="item-text text-center">
+								<h2>Lorem Lipsum</h2>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morlci non pretium metus, sit amet cursus nisi. Nam turpis, rhoncus curus iaculi a
+								</p>
+							</div>
+						</div>
+						<div class="span4 body-box-bottom-item">
+							<div class="item-img text-center">
+								<img class="header-top-logo img-responsive" src="../templates/hostgator/images/icon-bottom-3.png" alt=""/>
+							</div>
+							<div class="item-text text-center">
+								<h2>Lorem Lipsum</h2>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morlci non pretium metus, sit amet cursus nisi. Nam turpis, rhoncus curus iaculi a
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</article>
+<!-------------------------------------------------------->
+
+
+	</div><!-- end body -->
 	<!-- Footer -->
 	<footer class="footer" role="contentinfo">
-		<div class="container<?php echo ($params->get('fluidContainer') ? '-fluid' : ''); ?>">
-			<hr />
-			<jdoc:include type="modules" name="footer" style="none" />
-			<p class="pull-right">
-				<a href="#top" id="back-top">
-					<?php echo JText::_('TPL_PROTOSTAR_BACKTOTOP'); ?>
-				</a>
-			</p>
-			<p>
-				&copy; <?php echo date('Y'); ?> <?php echo $sitename; ?>
-			</p>
+		<div class="container">
+			<div class="row" style="margin-left:-30px;">
+				<div class="span8">
+					<div style="color: #fdc04c;">Site map | term of service | Privacy Policy</div>
+					<div style="color: #fff;">Copyright &copy; <?php echo date('Y'); ?> <?php echo $sitename; ?></div>
+				</div>
+				<div class="span4 text-right">
+					modsd
+				</div>
+			</div>
 		</div>
 	</footer>
 	<jdoc:include type="modules" name="debug" style="none" />
